6992ebdf86467586604ce77d82350a1ae538a1c1
CleberViniciusJala <141876206+CleberVinicius@users.noreply.github.com>
2023-08-17 10:40:33 -0300

    dino com tudo implementado 100% ok!

--- dino.py
+++ dino.py
@@ -1,17 +1,17 @@
-import pygame
-from pygame.locals import *
-from sys import exit
+import pygame  #  importa o módulo principal
+from pygame.locals import *    # importa várias constantes e eventos do Pygame 
+from sys import exit #importa a função sair, é usada para encerrar o programa 
 import os
 from random import randrange, choice
 
 pygame.init()
 pygame.mixer.init()
-
-diretorio_principal = os.path.dirname(__file__)
-diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
+                                                 #diretorio de aqrquivos das imagens e dos sons do jogo 
+diretorio_principal = os.path.dirname(__file__)  
+diretorio_imagens = os.path.join(diretorio_principal, 'imagens') 
 diretorio_sons = os.path.join(diretorio_principal, 'sons')
 
-LARGURA = 640
+LARGURA = 640         #parametros da tela do jogo 
 ALTURA = 460
     
 BRANCO = (255,215,0)
@@ -20,12 +20,12 @@ tela = pygame.display.set_mode((LARGURA, ALTURA))
 
 pygame.display.set_caption('Dino Game')
 
-sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
+sprite_sheet = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha() 
 
-som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'death_sound.wav'))
+som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'death_sound.wav')) #som quando o personagem morre
 som_colisao.set_volume(1)
 
-som_pontuacao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'score_sound.wav'))
+som_pontuacao = pygame.mixer.Sound(os.path.join(diretorio_sons, 'score_sound.wav')) #implementação do som da pontuação 
 som_pontuacao.set_volume(1)
 
 colidiu = False
@@ -35,7 +35,7 @@ escolha_obstaculo = choice([0, 1])
 pontos = 0 
 
 velocidade_jogo = 10
-imagen_defundo=pygame.image.load("imagens/cenariofundo.png")
+imagen_defundo=pygame.image.load("imagens/cenariofundo.png") #implementação do Cenario de fundo do Jogo
 
 def exibe_mensagem(msg, tamanho, cor):
     fonte = pygame.font.SysFont('comicsansms', tamanho, True, False)
@@ -43,7 +43,7 @@ def exibe_mensagem(msg, tamanho, cor):
     texto_formatado = fonte.render(mensagem, True, cor)
     return texto_formatado
 
-def reiniciar_jogo():
+def reiniciar_jogo(): # reinicialização do jogo 
     global pontos, velocidade_jogo, colidiu, escolha_obstaculo
     pontos = 0
     velocidade_jogo = 10
@@ -54,10 +54,10 @@ def reiniciar_jogo():
     cacto.rect.x = LARGURA
     escolha_obstaculo = choice([0, 1])
 
-class Dino(pygame.sprite.Sprite): 
+class Dino(pygame.sprite.Sprite): #implementação do personagem do Metal Slug spritesheet parte por parte 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.som_pulo = pygame.mixer.Sound(os.path.join(diretorio_sons, 'jump_sound.wav'))
+        self.som_pulo = pygame.mixer.Sound(os.path.join(diretorio_sons, 'jump_sound.wav')) #implementação ddo som de pular
         self.som_pulo.set_volume(1)
         self.imagempersonagem = []
         self.imagempersonagem.append(pygame.image.load('imagens/imagempersonagemcorrendo0.png'))
@@ -74,18 +74,18 @@ class Dino(pygame.sprite.Sprite):
         self.image = self.imagempersonagem[self.index_lista]
         self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
-        self.pos_y_inicial = ALTURA - 25 - 96//2
+        self.pos_y_inicial = ALTURA - 40 - 96//2
         self.rect.topleft = (100, self.pos_y_inicial) #368   416(centro y)
         self.pulo = False
-
+        self.image = pygame.transform.scale(self.image,(34*1.5,37*1.5)) #aumento do personagem que mutiplica o tamanho da imagem por 1,5
     def pular(self):
         self.pulo = True
         self.som_pulo.play()
 
-    def update(self):
+    def update(self): #atualiza a posição do objeto na tela para indicar que ele esta se movendo 
 
         if self.pulo == True:
-            if self.rect.y <= self.pos_y_inicial - 150:
+            if self.rect.y <= self.pos_y_inicial - 110:
                 self.pulo = False
             self.rect.y -= 15
 
@@ -100,8 +100,8 @@ class Dino(pygame.sprite.Sprite):
             self.index_lista = 0
         self.index_lista += 0.25
         self.image = self.imagempersonagem[int(self.index_lista)]
-
-class Nuvens(pygame.sprite.Sprite):
+        self.image = pygame.transform.scale(self.image,(34*1.5,37*1.5))#aumento do personagem que mutiplica o tamanho da imagem por 1,5
+class Nuvens(pygame.sprite.Sprite): #implementação das Nuvens e das cores das nuvens utilizando o piskel app
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprite_sheet.subsurface((7*32, 0), (32,32))
@@ -110,13 +110,13 @@ class Nuvens(pygame.sprite.Sprite):
         self.rect.y = randrange(50, 200, 50)
         self.rect.x = LARGURA - randrange(30, 300, 90)
 
-    def update(self):
+    def update(self): #atualiza a posição do objeto na tela para indicar que ele esta se movendo 
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
             self.rect.y = randrange(50, 200, 50)
         self.rect.x -= velocidade_jogo
 
-class Chao(pygame.sprite.Sprite):
+class Chao(pygame.sprite.Sprite): #implementação das Chao e das cores das nuvens utilizando o piskel app
     def __init__(self, pos_x):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprite_sheet.subsurface((6*32, 0), (32,32))
@@ -125,12 +125,12 @@ class Chao(pygame.sprite.Sprite):
         self.rect.y = ALTURA - 64
         self.rect.x = pos_x * 64
 
-    def update(self):
+    def update(self): #atualiza a posição do objeto na tela para indicar que ele esta se movendo 
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
         self.rect.x -= 10
     
-class Cacto(pygame.sprite.Sprite):   
+class Cacto(pygame.sprite.Sprite):   #implementação das Cacto e das cores das nuvens utilizando o piskelapp
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = sprite_sheet.subsurface((5*32, 0), (32,32))
@@ -141,7 +141,7 @@ class Cacto(pygame.sprite.Sprite):
         self.rect.center = (LARGURA,  ALTURA - 64)
         self.rect.x = LARGURA
 
-    def update(self):
+    def update(self): #atualiza a posição do objeto na tela para indicar que ele esta se movendo 
         if self.escolha == 0:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
@@ -164,7 +164,7 @@ class DinoVoador(pygame.sprite.Sprite):
         self.rect.center = (LARGURA, 300)
         self.rect.x = LARGURA
     
-    def update(self):
+    def update(self):  #atualiza a posição do objeto na tela para indicar que ele esta se movendo 
         if self.escolha == 1:
             if self.rect.topright[0] < 0:
                 self.rect.x = LARGURA
@@ -175,7 +175,7 @@ class DinoVoador(pygame.sprite.Sprite):
             self.index_lista += 0.25
             self.image = self.imagens_dinossauro[int(self.index_lista)]
 
-todas_as_sprites = pygame.sprite.Group()
+todas_as_sprites = pygame.sprite.Group() #adicionei todas as sprites ao Jogo
 dino = Dino()
 todas_as_sprites.add(dino)
 
@@ -187,17 +187,17 @@ for i in range(LARGURA*2//64):
     chao = Chao(i)
     todas_as_sprites.add(chao)
 
-cacto = Cacto()
+cacto = Cacto() #sprite do Cacto 
 todas_as_sprites.add(cacto)
 
-dino_voador = DinoVoador()
+dino_voador = DinoVoador() #sprite do DinoVoador
 todas_as_sprites.add(dino_voador)
 
-grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos = pygame.sprite.Group() #sprite dos obstaculos
 grupo_obstaculos.add(cacto)
 grupo_obstaculos.add(dino_voador)
 
-relogio = pygame.time.Clock()
+relogio = pygame.time.Clock() # controla a quantidade de frames por segundo do jogo
 while True:
     tela.blit(imagen_defundo,[0,0])
     relogio.tick(30)
@@ -233,9 +233,9 @@ while True:
     if colidiu == True:
         if pontos % 100 == 0:
             pontos += 1
-        game_over = exibe_mensagem('Você Perdeu', 40, (0,0,0))
+        game_over = exibe_mensagem('GAME OVER', 40, (255,255,255)) #exibe a mensagem em cor Branca 
         tela.blit(game_over, (LARGURA//2, ALTURA//2))
-        restart = exibe_mensagem('Pressione R para reiniciar', 20, (0,0,0))
+        restart = exibe_mensagem('Pressione R para reiniciar', 20, (255,255,255)) #exibe a mensagem em cor Branca
         tela.blit(restart, (LARGURA//2, (ALTURA//2) + 60))
 
     else:
